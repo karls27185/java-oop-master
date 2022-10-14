793d3ed1134842d9d0ef7ed6705d20322cefe6ae
karls27185 <karls27185@gmail.com>
2022-10-14 18:10:59 +0300

    test

---
+++ src/academy/devonline/java/structures/DataStorage.java
@@ -0,0 +1,14 @@
+package academy.devonline.java.structures;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public interface DataStorage {
+
+    void add(int value);
+
+    int get();
+
+    int size();
+}
---
+++ src/academy/devonline/java/structures/DataStorageTest.java
@@ -0,0 +1,23 @@
+package academy.devonline.java.structures;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+class DataStorageTest {
+
+    public static void main(String[] args) {
+        DataStorage dataStorage = null; // new Stack() or new Queue();
+
+        for (int i = 0; i < 5; i++) {
+            dataStorage.add(i);
+        }
+
+        // 4 3 2 1 0 = for Stack
+        // 0 1 2 3 4 = for Queue
+        while (dataStorage.size() > 0){
+            System.out.print(dataStorage.get() + " ");
+        }
+        System.out.println();
+    }
+}
