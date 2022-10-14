3d6e6198fb71af8032e3f32d3e417a9d70dac518
karls27185 <karls27185@gmail.com>
2022-10-15 02:57:20 +0300

    test

---
+++ src/academy/devonline/java/structures/QueueBasedOnArray.java
@@ -0,0 +1,52 @@
+package academy.devonline.java.structures;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public class QueueBasedOnArray implements DataStorage {
+
+    private int[] array;
+
+    private int size;
+
+    public QueueBasedOnArray(int size) {
+        array = new int[size];
+    }
+
+    public QueueBasedOnArray() {
+        this(5);
+    }
+
+    @Override
+    public void add(int value) {
+        if (size == array.length) {
+            grow(array.length == 0 ? 5 : array.length * 2);
+        }
+        array[size++] = value;
+    }
+
+    private void grow(int newSize) {
+        int[] newArray = new int[newSize];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+
+    @Override
+    public int get() {
+        if (size > 0){
+            int result = array[0];
+            System.arraycopy(array, 1, array, 0, array.length - 1);
+            size--;
+            return result;
+        }else {
+            // TODO throw Exception
+            return 0;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
