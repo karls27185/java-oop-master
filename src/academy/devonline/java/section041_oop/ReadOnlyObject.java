1a7c7d4e7ad078250f5fc9b0260089a181973214
karls27185 <karls27185@gmail.com>
2022-10-26 23:03:45 +0300

    13.017

---
+++ src/academy/devonline/java/section041_oop/ReadOnlyObject.java
@@ -0,0 +1,19 @@
+package academy.devonline.java.section041_oop;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+
+public class ReadOnlyObject {
+
+    private String value;
+
+    public ReadOnlyObject(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
