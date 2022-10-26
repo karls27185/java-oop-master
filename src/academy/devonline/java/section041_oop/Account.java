f40948925482cbe3dee7c82511d79098fdd4fa99
karls27185 <karls27185@gmail.com>
2022-10-26 22:56:02 +0300

    13.015

---
+++ src/academy/devonline/java/section041_oop/Account.java
@@ -0,0 +1,71 @@
+package academy.devonline.java.section041_oop;
+
+import java.time.Instant;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+
+public class Account {
+
+    private String login;
+
+    private String email;
+
+    private char[] password;
+
+    private boolean active;
+
+    private Instant created;
+
+    private Instant updated;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public Instant getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Instant updated) {
+        this.updated = updated;
+    }
+}
