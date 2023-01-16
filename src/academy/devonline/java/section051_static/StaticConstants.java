674d9f103daa1fa29906b1a76027f647ddafb0c2
karls27185 <karls27185@gmail.com>
2023-01-16 23:47:46 +0300

    24.005

---
+++ src/academy/devonline/java/section051_static/StaticConstants.java
@@ -0,0 +1,18 @@
+package academy.devonline.java.section051_static;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class StaticConstants {
+
+    public static final double PI = 3.14;
+
+    public static void main(String[] args) {
+        System.out.println(StaticConstants.PI);
+
+        System.out.println(Math.PI);
+
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
