b5d795750f9c30bb6cd793245a19d222dbd3135e
karls27185 <karls27185@gmail.com>
2023-01-16 23:24:57 +0300

    24.004

--- src/academy/devonline/java/section051_static/UsingClassInsteadOfObject.java
+++ src/academy/devonline/java/section051_static/UsingClassInsteadOfObject.java
@@ -1,5 +1,7 @@
 package academy.devonline.java.section051_static;
 
+import java.util.Arrays;
+
 /**
  * @author Karl
  * @link https://babayan.keenetic.link/
@@ -8,11 +10,19 @@ public class UsingClassInsteadOfObject {
 
     private static int value;
 
+    public static int getValue(){
+        return value;
+    }
+
     public static void main(String[] args) {
         value = 5;
         UsingClassInsteadOfObject.value = 8;
 
         System.out.println(value);
         System.out.println(UsingClassInsteadOfObject.value);
+        System.out.println(getValue());
+        System.out.println(UsingClassInsteadOfObject.getValue());
+
+        System.out.println(Arrays.toString(new int[]{1, 2, 3}));
     }
 }
