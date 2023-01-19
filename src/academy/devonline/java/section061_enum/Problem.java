b8134c20da2566dca88babec082fcddc30129865
karls27185 <karls27185@gmail.com>
2023-01-20 02:44:58 +0300

    25.001

---
+++ src/academy/devonline/java/section061_enum/Problem.java
@@ -0,0 +1,19 @@
+package academy.devonline.java.section061_enum;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class Problem {
+    public static class Status{
+        // started
+
+        // in_progress
+
+        // completed
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
