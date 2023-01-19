aaf9ee10952da851e788a647ef9fcb039f8cfc8f
karls27185 <karls27185@gmail.com>
2023-01-20 01:58:33 +0300

    24.016

---
+++ src/academy/devonline/java/section051_static/StaticInnerClass.java
@@ -0,0 +1,16 @@
+package academy.devonline.java.section051_static;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class StaticInnerClass {
+
+    class NotStaticInner{ // имеет доступа к полям и методам внешнего класса
+
+    }
+
+    static class StaticInner { // не имеет доступа к полям и методам внешнего класса
+
+    }
+}
