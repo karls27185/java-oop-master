2af804be7f55e17c570b11149a00db9d0b4dd30f
karls27185 <karls27185@gmail.com>
2023-01-21 03:18:36 +0300

    25.004

---
+++ src/academy/devonline/java/section061_enum/Solution3.java
@@ -0,0 +1,43 @@
+package academy.devonline.java.section061_enum;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class Solution3 {
+
+//    public static final class Status {
+//        public static final Status STARTED = new Status();
+//        public static final Status IN_PROGRESS = new Status();
+//        public static final Status COMPLETED = new Status();
+//
+//        private Status() {
+//        }
+//    }
+
+    public enum Status {
+        STARTED,
+        IN_PROGRESS,
+        COMPLETED
+    }
+
+    public static class Task {
+        private Status status = Status.STARTED;
+
+        public void setStatus(final Status status) {
+            this.status = status;
+        }
+
+        public Status getStatus(Status status) {
+            return status;
+        }
+    }
+
+    public static void main(final String[] args) {
+        Task task = new Task();
+        //
+        task.setStatus(Status.IN_PROGRESS);
+        //
+        task.setStatus(Status.COMPLETED);
+    }
+}
