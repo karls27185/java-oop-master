81453dc00db3f23e52d5aad8390af3b56bd58d8a
karls27185 <karls27185@gmail.com>
2023-01-24 16:52:34 +0300

    26.007

--- src/academy/devonline/java/section071_class_types/Interface.java
+++ src/academy/devonline/java/section071_class_types/Interface.java
@@ -4,6 +4,6 @@ package academy.devonline.java.section071_class_types;
  * @author Karl
  * @link https://babayan.keenetic.link/
  */
-public interface Interace {
+public interface Interface {
     void method1();
 }
