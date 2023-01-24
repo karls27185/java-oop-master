80616a8e30d5ce663d5a8cc247cb2bbcb9074df4
karls27185 <karls27185@gmail.com>
2023-01-24 17:25:00 +0300

    26.009

---
+++ src/academy/devonline/java/section071_class_types/Enum.java
@@ -0,0 +1,11 @@
+package academy.devonline.java.section071_class_types;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public enum Enum {
+    VALUE_1,
+    VALUE_2,
+    VALUE_3
+}
