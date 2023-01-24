744ac9a5d1f90de679dfd29a50bd3d35546ae50b
karls27185 <karls27185@gmail.com>
2023-01-24 15:13:40 +0300

    26.005

---
+++ src/academy/devonline/java/section071_class_types/AbstractClass.java
@@ -0,0 +1,9 @@
+package academy.devonline.java.section071_class_types;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public abstract class AbstractClass {
+    public abstract void method();
+}
