5ee144a6c56920d812c97c34b89d4d764c4e9363
karls27185 <karls27185@gmail.com>
2023-01-24 15:03:43 +0300

    Revert "25.003"
    This reverts commit d02b63ca98ff108c758cc7ca2d9f6c36ab4aa1e1.

--- src/academy/devonline/java/section071_class_types/NotPublicClass.java
+++
@@ -1,8 +0,0 @@
-package academy.devonline.java.section071_class_types;
-
-/**
- * @author Karl
- * @link https://babayan.keenetic.link/
- */
-class NotPublicClass {
-}
--- src/academy/devonline/java/section071_class_types/PublicClass.java
+++
@@ -1,8 +0,0 @@
-package academy.devonline.java.section071_class_types;
-
-/**
- * @author Karl
- * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
- */
-public class PublicClass {
-}
