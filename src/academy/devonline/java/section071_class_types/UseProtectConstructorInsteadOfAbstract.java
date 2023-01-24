a6ff4d3b25d737ec8e863f0e0de3b6c2d08c4492
karls27185 <karls27185@gmail.com>
2023-01-24 15:17:13 +0300

    26.006

---
+++ src/academy/devonline/java/section071_class_types/UseProtectConstructorInsteadOfAbstract.java
@@ -0,0 +1,9 @@
+package academy.devonline.java.section071_class_types;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public class UseProtectConstructorInsteadOfAbstract {
+    protected UseProtectConstructorInsteadOfAbstract(){}
+}
