d02b63ca98ff108c758cc7ca2d9f6c36ab4aa1e1
karls27185 <karls27185@gmail.com>
2023-01-24 14:34:28 +0300

    25.003

---
+++ src/academy/devonline/java/section071_class_types/NotPublicClass.java
@@ -0,0 +1,8 @@
+package academy.devonline.java.section071_class_types;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+class NotPublicClass {
+}
---
+++ src/academy/devonline/java/section071_class_types/PublicClass.java
@@ -0,0 +1,8 @@
+package academy.devonline.java.section071_class_types;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class PublicClass {
+}
