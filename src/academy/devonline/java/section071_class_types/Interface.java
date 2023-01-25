2611c348a07aab2aded6d4077dd86ff4ce5df4a7
karls27185 <karls27185@gmail.com>
2023-01-26 01:57:05 +0300

    26.016

--- src/academy/devonline/java/section071_class_types/Interface.java
+++ src/academy/devonline/java/section071_class_types/Interface.java
@@ -4,8 +4,10 @@ package academy.devonline.java.section071_class_types;
  * @author Karl
  * @link https://babayan.keenetic.link/
  */
+@FunctionalInterface
 public interface Interface {
     void method1();
 
-    static class Inner{}
+//    void m();
+    class Inner{}
 }
