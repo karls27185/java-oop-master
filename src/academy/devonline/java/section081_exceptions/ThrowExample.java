c40f8b6224055b2785aad57dfe2326d12417744e
karls27185 <karls27185@gmail.com>
2023-02-22 02:51:08 +0300

    28.006

---
+++ src/academy/devonline/java/section081_exceptions/ThrowExample.java
@@ -0,0 +1,18 @@
+package academy.devonline.java.section081_exceptions;
+
+import java.util.Scanner;
+
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
+public class ThrowExample {
+    public static void main(String[] args) {
+//        1) не явная исключительная ситуация
+//        System.out.println(2/0);
+//        2) исключительная ситуация внутри стандартного метода
+//        new Scanner(System.in).nextInt();
+//        3) выброс исключений явным образом
+//        throw new IllegalArgumentException("qwe");
+    }
+}
