aa8e39ec897b41a708293fc82ad9c2a7cfc35b0c
karls27185 <karls27185@gmail.com>
2023-02-26 02:45:02 +0300

    28.012

--- src/academy/devonline/java/section081_exceptions/MultiCatchExample.java
+++ src/academy/devonline/java/section081_exceptions/MultiCatchExample.java
@@ -1,17 +1,33 @@
 package academy.devonline.java.section081_exceptions;
 
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * @author Karl
+ * @link <a href="https://babayan.keenetic.link/">https://babayan.keenetic.link</a>
+ */
 public class MultiCatchExample {
-    public static void main(String[] args) throws ClassNotFoundException {
-        doSomething();
+    public static void main(String[] args) {
+        try {
+            doSomething();
+        } catch (InputMismatchException e) {
+            System.err.println("InputMismatchException: " + e.getMessage());
+        } catch (NoSuchElementException e) {
+            System.err.println("NoSuchElementException: " + e.getMessage());
+        } catch (IllegalStateException e) {
+            System.err.println("IllegalStateException: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("IllegalArgumentException: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.err.println("ArithmeticException: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("ClassNotFoundException: " + e.getMessage());
+        }
+
         System.out.println("After doSomething()");
     }
 
     /**
-     *
      * @throws InputMismatchException
      * @throws NoSuchElementException
      * @throws IllegalStateException
@@ -19,17 +35,18 @@ public class MultiCatchExample {
      * @throws ArithmeticException
      * @throws ClassNotFoundException
      */
+
     private static void doSomething() throws ClassNotFoundException {
         final int value = new Scanner(System.in).nextInt();
-        if (value < 0){
+        if (value < 0) {
             throw new IllegalArgumentException("value < 0");
         }
-        if (value == 0){
+        if (value == 0) {
             throw new ArithmeticException("value = 0!");
         }
-        if (value == 5){
+        if (value == 5) {
             throw new ClassNotFoundException("value = 5");
         }
-        System.out.println("ok");
+        System.out.println("OK");
     }
 }
