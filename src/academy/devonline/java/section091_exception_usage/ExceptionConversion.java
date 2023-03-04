bd8923932be4c9777fe9025408721ef3e56d3644
karls27185 <karls27185@gmail.com>
2023-03-04 03:43:20 +0300

    29.005

--- src/academy/devonline/java/section091_exception_usage/ExceptionConversion.java
+++ src/academy/devonline/java/section091_exception_usage/ExceptionConversion.java
@@ -33,8 +33,8 @@ public class ExceptionConversion {
                 aggregateException.addSuppressed(exception);
             }
         }
-        if (!success){
-            throw aggregateException;
+        if (!success) {
+            throw (CantReadConfigValueException) aggregateException.fillInStackTrace();
         }
         System.out.println(configValue);
     }
